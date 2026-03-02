3717dc4e4d88293ba1ad028e4bf5baf72fa2308c
Ch589814 <niyomufashadelphine1@gmail.com>
2026-03-02 11:37:13 +0200

    add Person.java with name, age, city and getter and setter methods

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Person {
+    private String name;
+    private int age;
+    private String city;
+
+    // Constructor
+    public Person(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
 }
